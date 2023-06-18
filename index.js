b1dc26eac3cfa9a5bb11bc663aa468c0b4e2478c
AnjuTewatia <anjutewatia008@gmail.com>
2023-06-18 16:38:52 +0530

    backend

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
+require("dotenv").config();
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-require("dotenv").config();
 const connection = require("./config/db");
 const { userrouter } = require("./models/userRoute");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
 
+app.use(cors());
 app.get("/", (req, res) => {
   res.send(" Welcome To The Home page");
 });
-
 app.use("/user", userrouter);
 
 app.listen(process.env.PORT, async () => {
